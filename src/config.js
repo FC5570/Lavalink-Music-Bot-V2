9951e9517ca2b3fa1107cb9eeeed59abfdbee14a
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-02-10 23:24:20 +0530

    ffs

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  token: "Nzg5MDY5NjQ0MjUzMTAyMDkw.X9ssXw.S6bXyrsMK9WF7gB0yaSOd5KuDLY",
-  prefix: ";",
+  token: "discord bot token",
+  prefix: "bot prefix",
   nodes: [
     {
-      host: "54.236.50.116",
-      password: "bubludiblu",
-      port: 6969,
+      host: "lavalink host",
+      password: "lavalink password",
+      port: "lavalink port",
     },
   ],
-  csID: "039c5c7618534b9eb04e5987c87e4fa6",
-  csSecret: "8628a00390494e3dbdbf5ac1f70b6768",
+  csID: "spotify client id",
+  csSecret: "spotify client secret",
 };
